50eed7bb25b0a59659fdaf4f215c863e05e6ea51
demesameneshoa <demesameneshoa@gmail.com>
2023-06-15 01:06:11 +0300

    Add calculator class and function with Tests

--- funcitons.test.js
+++ funcitons.test.js
@@ -10,3 +10,31 @@ test('Tests string length provided', () => {
 test('Testing string reverse function', () => {
   expect(myFunctions.reverseString('Test')).toBe('tseT');
 });
+
+const CalculatorObjectone = new myFunctions.Calculator(5, 6);
+const CalculatorObjecttwo = new myFunctions.Calculator(5, 0);
+const CalculatorObjectthree = new myFunctions.Calculator(0, 7);
+describe('Testing Calculator Functions', () => {
+  test('Testing Addition Function', () => {
+    expect(CalculatorObjectone.addition()).toBe(11);
+    expect(CalculatorObjecttwo.addition()).toBe(5);
+    expect(CalculatorObjectthree.addition()).toBe(7);
+  });
+  test('Testing Subtraction Function', () => {
+    expect(CalculatorObjectone.subtraction()).toBe(-1);
+    expect(CalculatorObjecttwo.subtraction()).toBe(5);
+    expect(CalculatorObjectthree.subtraction()).toBe(-7);
+  });
+  test('Testing MUltiplication Function', () => {
+    expect(CalculatorObjectone.multiplication()).toBe(30);
+    expect(CalculatorObjecttwo.multiplication()).toBe(0);
+    expect(CalculatorObjectthree.multiplication()).toBe(0);
+  });
+  test('Testing Division Function', () => {
+    expect(CalculatorObjectone.division()).toBe(0.83);
+    expect(() => {
+      CalculatorObjecttwo.division();
+    }).toThrow('cannot divide by zero');
+  });
+  expect(CalculatorObjectthree.division()).toBe(0);
+});
--- functions.js
+++ functions.js
@@ -9,7 +9,36 @@ function stringLength(string) {
 function reverseString(string) {
   return string.split('').reverse().join('');
 }
+
+class Calculator {
+  constructor(num1, num2) {
+    this.num1 = num1;
+    this.num2 = num2;
+  }
+
+  addition() {
+    return this.num1 + this.num2;
+  }
+
+  subtraction() {
+    return this.num1 - this.num2;
+  }
+
+  multiplication() {
+    return this.num1 * this.num2;
+  }
+
+  division() {
+    if (this.num2 === 0) {
+      throw new Error('cannot divide by zero');
+    } else {
+      return parseFloat((this.num1 / this.num2).toFixed(2));
+    }
+  }
+}
+
 module.exports = {
   stringLength,
   reverseString,
+  Calculator,
 };
